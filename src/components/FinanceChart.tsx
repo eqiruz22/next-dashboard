46c7a97f5abc71ae2ff0d535c0aa5dbdcd96ad82
eqiruz22 <ahmadrifa11620@gmail.com>
2024-10-22 13:55:02 +0700

    first init

---
+++ src/components/FinanceChart.tsx
@@ -0,0 +1,115 @@
+"use client";
+
+import React from "react";
+import Image from "next/image";
+import {
+  CartesianGrid,
+  Legend,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+
+const data = [
+  {
+    name: "Jan",
+    income: 54000,
+    expense: 2400,
+  },
+  {
+    name: "Feb",
+    income: 3000,
+    expense: 1398,
+  },
+  {
+    name: "Mar",
+    income: 2000,
+    expense: 9800,
+  },
+  {
+    name: "Apr",
+    income: 2780,
+    expense: 3908,
+  },
+  {
+    name: "Mei",
+    income: 1890,
+    expense: 4800,
+  },
+  {
+    name: "Jun",
+    income: 2390,
+    expense: 3800,
+  },
+  {
+    name: "Jul",
+    income: 3490,
+    expense: 4300,
+  },
+  {
+    name: "Aug",
+    income: 2780,
+    expense: 3908,
+  },
+  {
+    name: "Sept",
+    income: 1890,
+    expense: 4800,
+  },
+  {
+    name: "Oct",
+    income: 2390,
+    expense: 3800,
+  },
+  {
+    name: "Nov",
+    income: 3490,
+    expense: 4300,
+  },
+  {
+    name: "Dec",
+    income: 4000,
+    expense: 5000,
+  },
+];
+
+const FinanceChart = () => {
+  return (
+    <div className="bg-white rounded-lg p-4 h-full">
+      <div className="flex justify-between items-center">
+        <h1 className="text-lg font-semibold">Finance</h1>
+        <Image src={"/moreDark.png"} alt="" width={20} height={20} />
+      </div>
+      <ResponsiveContainer width="100%" height="90%">
+        <LineChart
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line
+            type="monotone"
+            dataKey="income"
+            stroke="#8884d8"
+            activeDot={{ r: 8 }}
+          />
+          <Line type="monotone" dataKey="expense" stroke="#82ca9d" />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
+  );
+};
+
+export default FinanceChart;
